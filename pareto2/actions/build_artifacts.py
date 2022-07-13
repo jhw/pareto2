ce21a8ec00e8e8002d7221c78d72485bbb68a168
Justin Worrall <justin.worrall@gmail.com>
2022-07-13 16:06:20 +0100

    removed layer validation

--- pareto2/actions/build_artifacts.py
+++ pareto2/actions/build_artifacts.py
@@ -3,20 +3,10 @@ from pareto2.core.template import Template
 
 from pareto2.actions.lambdas import Lambdas
 from pareto2.actions.layers import Layers
-from pareto2.actions.parameters import Parameters
 
 from botocore.exceptions import ClientError, WaiterError
 
 import boto3, yaml
-
-def has_internet():
-    import http.client as httplib
-    conn=httplib.HTTPSConnection("8.8.8.8", timeout=5)
-    try:
-        conn.request("HEAD", "/")
-        return True
-    except:
-        return False
 
 if __name__=="__main__":
     try:
@@ -26,38 +16,31 @@ if __name__=="__main__":
         if len(sys.argv) < 2:
             raise RuntimeError("please enter deploy stage")
         stagename=sys.argv[1]
-        print ("initialising/validating metadata")
+        # initialising/validating metadata
         from datetime import datetime
         timestamp=datetime.utcnow().strftime("%Y-%m-%d-%H-%M-%S")
         from pareto2.cli import load_config
         config=load_config()
         md=Metadata.initialise(stagename)
         md.validate().expand()
-        print ("initialising/validating lambdas")
+        # initialising/validating lambdas
         lambdas=Lambdas.initialise(md=md,
                                    timestamp=timestamp)
         lambdas.validate()
         lambdas.dump_zip()
         config.update({"StageName": stagename,
                        "ArtifactsKey": lambdas.s3_key_zip})
-        print ("initialising/validating layers")
+        # initialising/validating layers
         s3=boto3.client("s3")
         layers=Layers.initialise(md)
-        if has_internet():
-            layers.validate(s3, config)
-        print ("initialising/validating template")
+        # initialising/validating template
         from pareto2.core import init_template
         template=init_template(md,
                                name="main",
                                timestamp=timestamp)
-        print ()
-        print (yaml.safe_dump(template.metrics,
-                              default_flow_style=False))
+        print ("dumping to %s" % template.filename_json)
         template.dump_json(template.filename_json)
         template.validate_root()
-        params=Parameters.initialise([config,
-                                      layers.parameters])
-        params.validate(template)
     except RuntimeError as error:
         print ("Error: %s" % str(error))
     except ClientError as error:
--- pareto2/actions/layers.py
+++ pareto2/actions/layers.py
@@ -9,28 +9,6 @@ class Layers(list):
     def __init__(self, items=[]):
         list.__init__(self, items)
 
-    def list_artifacts(self, s3, bucketname,
-                       prefix="layer"):
-        paginator=s3.get_paginator("list_objects_v2")
-        pages=paginator.paginate(Bucket=bucketname,
-                                 Prefix=prefix)
-        keys=[]
-        for struct in pages:
-            if "Contents" in struct:
-                for obj in struct["Contents"]:
-                    keys.append(obj["Key"])
-        return keys
-        
-    def validate(self, s3, config):
-        keys=self.list_artifacts(s3, config["ArtifactsBucket"])
-        errors=[]
-        for pkgname in self:
-            key="layer-%s.zip" % pkgname
-            if key not in keys:
-                errors.append(pkgname)
-        if errors!=[]:
-            raise RuntimeError("no layer artifacts for %s" % ", ".join(errors))
-
     @property
     def parameters(self):
         return {hungarorise("layer-key-%s" % pkgname): "layer-%s.zip" % pkgname
