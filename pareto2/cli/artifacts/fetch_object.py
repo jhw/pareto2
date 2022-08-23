e916122494983531aab869de033d017fb1956170
Justin Worrall <justin.worrall@gmail.com>
2022-08-23 15:01:07 +0100

    removed superfluous list_contents

--- pareto2/cli/artifacts/fetch_object.py
+++ pareto2/cli/artifacts/fetch_object.py
@@ -4,19 +4,6 @@ from pareto2.cli import *
 
 import boto3
 
-def list_contents(s3, bucketname, prefix):
-    paginator=s3.get_paginator("list_objects_v2")
-    kwargs={"Bucket": bucketname}
-    if prefix:
-        kwargs["Prefix"]=prefix
-    pages=paginator.paginate(**kwargs)
-    for struct in pages:
-        if "Contents" in struct:
-            for obj in struct["Contents"]:
-                print ("%s\t%s\t%s" % (obj["LastModified"],
-                                       obj["Size"],
-                                       obj["Key"]))
-    
 if __name__=="__main__":
     try:
         import sys
