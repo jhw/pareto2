688608299536876e4f294d48352b21fcf457a512
Justin Worrall <justin.worrall@gmail.com>
2022-09-05 09:16:59 +0100

    bumped to 0.2.10

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def format_dependencies(root="requirements.txt"):
 
 setuptools.setup(
     name="pareto2",
-    version="0.2.9",
+    version="0.2.10",
     author="jhw",
     author_email="justin.worrall@gmail.com",
     description="OTP for serverless",
