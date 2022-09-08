f8e9555737a27221b8dfb3452b7fdde16418e345
Justin Worrall <justin.worrall@gmail.com>
2022-09-08 09:34:35 +0100

    cleaned up setenv

--- setenv.sh
+++ setenv.sh
@@ -4,13 +4,5 @@ export AWS_PROFILE=woldeploy
 export AWS_REGION=eu-west-1
 export PYTHONPATH=.
 
-export PARETO2_APP_NAME=demo
-
-if [ ! -L demo ]; then
-    ln -s ../pareto2-demo/config.yaml config.yaml
-    ln -s ../pareto2-demo/demo demo
-    ln -s ../pareto2-demo/fixtures fixtures
-    ln -s ../pareto2-demo/scripts scripts
-fi
 
 
