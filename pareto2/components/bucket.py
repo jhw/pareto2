ce037443fa974a4ef8d717fc706e2b334610b84b
Justin Worrall <justin.worrall@gmail.com>
2022-10-22 15:25:13 +0100

    updated bucket main

--- pareto2/components/bucket.py
+++ pareto2/components/bucket.py
@@ -20,13 +20,26 @@ def render_outputs(bucket):
 
 if __name__=="__main__":
     try:
+        import os, sys
+        filename=sys.argv[1] if len(sys.argv) > 1 else "config.yaml"
+        if not os.path.exists(filename):
+            raise RuntimeError("%s does not exist" % filename)
         from pareto2.dsl import Config
-        config=Config.init_file(filename="demo.yaml")
+        config=Config.init_file(filename=filename)
         from pareto2.template import Template
         template=Template()
         for bucket in config["components"].buckets:
             template.resources.update(render_resources(bucket))
             template.outputs.update(render_outputs(bucket))
         print (template.render())
+        print ()
+        template.init_implied_parameters()
+        for validator in [template.parameters.validate,
+                          template.validate]:
+            try:
+                validator()
+            except RuntimeError as error:
+                print ("Warning: %s" % str(error))
+
     except RuntimeError as error:
         print ("Error: %s" % str(error))
