717783c3c23eaf7a70abc4b6f14393d9d5217cb4
Justin Worrall <justin.worrall@gmail.com>
2023-09-23 08:13:15 +0100

    added separate domain stuff

--- pareto2/components/api/__init__.py
+++ pareto2/components/api/__init__.py
@@ -9,6 +9,8 @@
 from pareto2.components import hungarorise as H
 from pareto2.components import resource
 
+from pareto2.components.api.domain import init_domain, init_domain_path_mapping, init_domain_record_set
+
 import json, yaml
 
 MethodArn="arn:aws:apigateway:${AWS::Region}:lambda:path/2015-03-31/functions/${arn}/invocations"
@@ -227,47 +229,6 @@ def init_model(api, endpoint, schematype=EndpointSchemaVersion):
     return (resourcename,
             "AWS::ApiGateway::Model",
             props)    
-
-@resource
-def init_domain(api):
-    resourcename=H("%s-api-domain" % api["name"])
-    domainname={"Fn::Sub": ["${prefix}.${name}", {"prefix": {"Ref": H("%s-domain-prefix" % api["name"])}, "name": {"Ref": H("domain-name")}}]}
-    props={"DomainName": domainname,
-           "CertificateArn": {"Ref": H("certificate-arn")}}
-    return (resourcename,
-            "AWS::ApiGateway::DomainName",
-            props)
-
-@resource
-def init_domain_path_mapping(api):
-    resourcename=H("%s-api-domain-path-mapping" % api["name"])
-    domainname={"Fn::Sub": ["${prefix}.${name}", {"prefix": {"Ref": H("%s-domain-prefix" % api["name"])}, "name": {"Ref": H("domain-name")}}]}
-    props={"DomainName": domainname,
-           "RestApiId": {"Ref": H("%s-api-rest-api" % api["name"])},
-           "Stage": api["stage"]["name"]}
-    depends=[H("%s-api-domain" % api["name"])]
-    return (resourcename,
-            "AWS::ApiGateway::BasePathMapping",
-            props,
-            depends)
-
-@resource
-def init_domain_record_set(api):
-    resourcename=H("%s-api-domain-record-set" % api["name"])
-    hzname={"Fn::Sub": ["${name}.", {"name": {"Ref": H("domain-name")}}]} # NB note trailing period
-    domainname={"Fn::Sub": ["${prefix}.${name}", {"prefix": {"Ref": H("%s-domain-prefix" % api["name"])}, "name": {"Ref": H("domain-name")}}]}
-    dnsname={"Fn::GetAtt": [H("%s-api-domain" % api["name"]), "DistributionDomainName"]}
-    hzid={"Fn::GetAtt": [H("%s-api-domain" % api["name"]), "DistributionHostedZoneId"]}
-    aliastarget={"DNSName": dnsname,
-                 "EvaluateTargetHealth": False,
-                 "HostedZoneId": hzid}
-    props={"HostedZoneName": hzname,
-           "Name": domainname, # NB `Name` key
-           "Type": "A",
-           "AliasTarget": aliastarget}
-    return (resourcename,
-            "AWS::Route53::RecordSet",
-            props)
 
 """
 - NB an open API is still CORS- enabled
