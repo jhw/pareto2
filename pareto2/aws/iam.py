25943d716ae2b3b789c3ae1e1798f3db751ce621
Justin Worrall <justin.worrall@gmail.com>
2024-03-08 17:09:07 +0000

    cleaned up role

--- pareto2/aws/iam.py
+++ pareto2/aws/iam.py
@@ -3,28 +3,32 @@ from pareto2.aws import Resource
 
 class Role(Resource):
 
-    def __init__(self, component_name, permissions=None, service=None):
-        self.component_name = component_name
-        self.permissions = permissions or []
+    def __init__(self, name, permissions=[], service="lambda.amazonaws.com"):
+        self.name = name
+        self.permissions = permissions
         self.service = service
 
     @property
     def aws_properties(self):
-        policy_name = H(f"{self.resource_name}-policy-${{AWS::StackName}}")
-        policy_document = self._policy_document(self.permissions)
-        policies = [{"PolicyDocument": policy_document, "PolicyName": policy_name}]
-        assume_role_policy_document = self._assume_role_policy_document()
+        policy_name = H(f"{self.name}-policy-${{AWS::StackName}}")
+        policies = [{"PolicyDocument": self.policy_document,
+                     "PolicyName": policy_name}]
         return {
-            "AssumeRolePolicyDocument": assume_role_policy_document,
+            "AssumeRolePolicyDocument": self.assume_role_policy_document,
             "Policies": policies
         }
 
-    def _policy_document(self, permissions):
-        # Simulate policy_document function's behavior
-        return {"Version": "2012-10-17", "Statement": [{"Action": permissions, "Effect": "Allow", "Resource": "*"}]}
+    @property
+    def policy_document(self):
+        return {"Version": "2012-10-17",
+                "Statement": [{"Action": self.permissions,
+                               "Effect": "Allow",
+                               "Resource": "*"}]}
 
-    def _assume_role_policy_document(self, service=None):
-        # Simulate assume_role_policy_document function's behavior, optionally for a specific service
-        principal_service = service or "sts.amazonaws.com"
-        return {"Version": "2012-10-17", "Statement": [{"Effect": "Allow", "Principal": {"Service": principal_service}, "Action": "sts:AssumeRole"}]}
+    @property
+    def assume_role_policy_document(self):
+        return {"Version": "2012-10-17",
+                "Statement": [{"Effect": "Allow",
+                               "Principal": {"Service": self.service},
+                               "Action": "sts:AssumeRole"}]}
 
