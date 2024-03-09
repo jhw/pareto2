78efe01be926bf512a8db2bc78b9da2802d28021
Justin Worrall <justin.worrall@gmail.com>
2024-03-09 15:07:50 +0000

    removed other components

--- pareto2/components/timer/__init__.py
+++
@@ -1,9 +0,0 @@
-from pareto2.aws.lambda import Permission as PermissionBase
-
-class Permission(PermissionBase):
-
-    def __init__(self, timer):
-        super().__init__(timer["name"],
-                         timer["action"],
-                         {"Fn::GetAtt": [f"{timer['name']}-timer", "Arn"]},
-                         "events.amazonaws.com")
