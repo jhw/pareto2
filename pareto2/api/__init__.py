7c5ccc8f654102b1645663d8cb6f72a61fc70eb0
Justin Worrall <justin.worrall@gmail.com>
2024-04-06 16:52:24 +0100

    added path to file loader

--- pareto2/api/__init__.py
+++ pareto2/api/__init__.py
@@ -6,13 +6,13 @@ def file_loader(pkg_root, root_dir=''):
     for root, dirs, files in os.walk(pkg_full_path):
         dirs[:] = [d for d in dirs if d != '__pycache__']
         for file in files:
-            if file.endswith('.pyc'):
-                continue
-            full_path = os.path.join(root, file)
-            with open(full_path, 'r', encoding='utf-8') as f:
-                content = f.read()
-                relative_path = os.path.relpath(full_path, root_dir)
-                file_contents.append((relative_path, content))
+            if (file == f"{pkg_root}/__init__.py" or
+                file.endswith("index.py")):
+                full_path = os.path.join(root, file)
+                with open(full_path, 'r', encoding='utf-8') as f:
+                    content = f.read()
+                    relative_path = os.path.relpath(full_path, root_dir)
+                    file_contents.append((relative_path, content))
     return file_contents
 
 if __name__ == "__main__":
