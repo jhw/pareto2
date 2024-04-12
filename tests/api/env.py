1d076a3f32ab06b70159769b53e2d01b52d6987b
Justin Worrall <justin.worrall@gmail.com>
2024-04-12 17:25:03 +0100

    completed mock lambda

--- tests/api/env.py
+++ tests/api/env.py
@@ -2,14 +2,19 @@ from pareto2.api.env import Env
 
 from tests.api import ApiTestBase, BucketName
 
-from moto import mock_s3
+from moto import mock_s3, mock_lambda
 
-import os, unittest
+import boto3, os, unittest
 
+LayerName = "whatevs"
+
+@mock_s3
+@mock_lambda
 class EnvTest(ApiTestBase):
 
     def setUp(self):
         super().setUp()
+        self.L = boto3.client("lambda")
     
     def test_environ(self):
         env = Env.create_from_environ()
@@ -20,11 +25,14 @@ class EnvTest(ApiTestBase):
         env = Env.create_from_bash(script)
         self.assertTrue("AwsProfile" in env)
 
-    def test_layers(self):
+    def test_layers(self, layer_name = LayerName):
+        self.L.publish_layer_version(LayerName = layer_name,
+                                     Content = {"ZipFile": b''})
         env = Env()
         env.update_layers()
         self.assertTrue(env != {})
 
+    """
     def test_certificates_1(self,
                             domain_name = "hello.spaas.link"):
         env = Env({"DomainName": domain_name})
@@ -40,9 +48,15 @@ class EnvTest(ApiTestBase):
         env.update_certificates()
         self.assertTrue("DistributionCertificateArn" in env)
         self.assertTrue("RegionalCertificateArn" in env)
+    """
         
-    def tearDown(self):
+    def tearDown(self, layer_name = LayerName):
         super().tearDown()
-       
+        for layer in self.L.list_layers()["Layers"]:
+            layername=layer["LayerName"]
+            version=layer["LatestMatchingVersion"]["Version"]
+            self.L.delete_layer_version(LayerName=layername,
+                                        VersionNumber=version)
+        
 if __name__ == "__main__":
     unittest.main()
