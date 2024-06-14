f7a3a49d0a9f32d427f1b090ea1d83f70b1061e5
Justin Worrall <justin.worrall@gmail.com>
2024-06-14 12:24:16 +0100

    udpated notes

--- pareto2/recipes/web_api/inline_code/custom_message.py
+++ pareto2/recipes/web_api/inline_code/custom_message.py
@@ -1,26 +1,5 @@
 """
-This stuff is the biggest nightmare ever
-AdminCreateUser requires username and code parameters
-But these are the names of the parameters at the pareto2 api level, for convenience
-Cognito requires different placeholders, as defined in request.usernameParameter and request.codeParameter (there is also request.linkParameter)
-And these differ from the pareto2 api placeholder names; in particular codeParameter is typically {####} not {code} (but {username} is {username})
-The handlers job is to insert these placeholders into the message, not the pareto2 api placeholders
-And not also you do not paste *values* into the template; you are inserting Cognito- compliant placeholders
-Cognito will then insert values for the placeholders into the template, after the handler has run
----
-Assuming this is done correctly, there's then the question of where the values come from
-I have no idea where code comes from, nor link
-username comes from event.userName
-But this doesn't always seem to be in username form, in fact mine look more like a UUID
-You can't seem to override this with an email address, if it is not an email address by default
-There are links on the web which say you should also urlquote the username value, which I think are incorrect
-Instead you have to make sure that your UserPool is configured to use email as user id
-And this seems to mean defining not just UsernameAttributes: email, but also UsernameConfiguration as well; without the latter, it seems userName is not guaranteed to be an email
-It's important to have logging enabled to you can see what messages Cognito is actually sending into the handler here
----
-
-If a message body isn't well formed then it will simply be silently ignored by Cognito
-No message is generated, so it's impossible to figure out why your custom message isn't being used; you will simply see the default message instead
+https://stackoverflow.com/a/78622562/124179
 """
 
 import logging, os
@@ -38,9 +17,6 @@ def validate_placeholders(message, required_placeholders):
         if f'{{{placeholder}}}' not in message:
             raise RuntimeError(f"Missing required placeholder: {{{placeholder}}}")
 
-"""
-"""
-        
 def handler(event, context):
     logger.info("Event [START]: %s", event)
     template_values = {'username': event['request']['usernameParameter'],
