1d6481431faf5b5b74e5feb0476a2f1919f6e464
Justin Worrall <justin.worrall@gmail.com>
2024-06-26 16:28:21 +0100

    added mock cognito

--- tests/recipes/inline_code/web_api/custom_attributes.py
+++ tests/recipes/inline_code/web_api/custom_attributes.py
@@ -1,10 +1,14 @@
 import unittest.mock as mock
 
+from moto import mock_cognitoidp
+
 import json, os, unittest
 
+@mock_cognitoidp
 class WebApiInlineCodeCustomAttributesTest(unittest.TestCase):
 
-    def setUp(self):        
+    def setUp(self):
+        self.setup_cognito()
         self.env = {}
         self.env['USER_CUSTOM_ATTRIBUTES'] = json.dumps([
             {
@@ -14,11 +18,17 @@ class WebApiInlineCodeCustomAttributesTest(unittest.TestCase):
             }
         ])
 
+    def setup_cognito(self):
+        pass
+
     def test_handler(self, event = {}):
         with mock.patch.dict(os.environ, self.env):
             from pareto2.recipes.web_api.inline_code.custom_attributes import handler
             handler(event, context = None)
 
+    def teardown_cognito(self):
+        pass
+            
     def tearDown(self):
         pass
             
