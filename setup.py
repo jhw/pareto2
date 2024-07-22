4a8ab6e79c573b25f1e5aa9c792dc1ff1f8866db
Justin Worrall <justin.worrall@gmail.com>
2024-07-22 14:26:38 +0100

    bumped to 0.8.76

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def filter_packages(root):
     
 setuptools.setup(
     name="pareto2",
-    version="0.8.75",
+    version="0.8.76",
     author="jhw",
     author_email="justin.worrall@gmail.com",
     description="An opinionated Cloudformation generator",
