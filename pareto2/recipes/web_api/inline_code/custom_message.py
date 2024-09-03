68c019d3a30877c15b4ecafd6fa57717f218976f
Justin Worrall <justin.worrall@gmail.com>
2024-09-03 15:30:12 +0100

    changed custom message to use chatgpt advised version and bumped to 0.8.82

--- pareto2/recipes/web_api/inline_code/custom_message.py
+++ pareto2/recipes/web_api/inline_code/custom_message.py
@@ -14,11 +14,13 @@ import os
 
 def replace_placeholders(message, template_values):
     for key, value in template_values.items():
-        message = message.replace(f'{{{key}}}', value)
+        # Use an empty string if the value is None
+        message = message.replace(f'{{{key}}}', value if value is not None else '')
     return message
 
 def handler(event, context):
     templates = json.loads(os.environ["EMAIL_TEMPLATES"])
+    
     if event['triggerSource'] == 'CustomMessage_AdminCreateUser':
         subject_body = templates["temp_password"]["subject"]
         message_body = templates["temp_password"]["message"]
@@ -27,9 +29,16 @@ def handler(event, context):
         message_body = templates["password_reset"]["message"]
     else:
         return event
-    template_values = {attr: event['request'][f"{attr}Parameter"]
-                       for attr in ["username", "code"]}
+    
+    # Safely get the parameters, defaulting to an empty string if not present
+    template_values = {
+        "username": event['request'].get('usernameParameter', ''),
+        "code": event['request'].get('codeParameter', '')
+    }
+    
     event['response']['emailSubject'] = replace_placeholders(subject_body, template_values)
     event['response']['emailMessage'] = replace_placeholders(message_body, template_values)
-    return event # NB Cognito Lambda handlers must return JSON
+    
+    return event  # Cognito Lambda handlers must return JSON
+
 
